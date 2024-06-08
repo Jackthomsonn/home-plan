fade35b690fb89ed037896e86a5b2973e1639b34
Jack Thomson <hello@jackthomson.co.uk>
2024-06-08 11:57:59 +0100

    Update

--- weather/handleWeather.go
+++ weather/handleWeather.go
@@ -1,10 +1,12 @@
 package weather
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func HandleWeather(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Weather endpoint hit")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 }
